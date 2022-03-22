d0df894902e8fa88a8f51b62b8fed11a81133b85
Anshu Raj <anshusopinion@gmail.com>
2022-03-22 15:59:15 +0530

    card layout done

--- src/pages/packages.tsx
+++ src/pages/packages.tsx
@@ -11,6 +11,7 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { BsCurrencyDollar } from 'react-icons/bs';
+import { FaArrowRight } from 'react-icons/fa';
 const packages = () => {
   const packagesList = [
     {
@@ -132,24 +133,60 @@ const packages = () => {
       <Heading>Choose a Tier to Gain Access to our Tools and Start</Heading>
       <Text>Earning Rewards</Text>
 
-      <Grid>
+      <Grid gridTemplateColumns="repeat(auto-fit,minmax(250px,1fr))" gap="4">
         {packagesList.map((item, i) => (
-          <GridItem key={item.name}>
-            <VStack>
-              <Heading as="h3" size="lg">
+          <GridItem
+            key={item.name}
+            boxShadow="xl"
+            borderRadius="lg"
+            overflow="hidden"
+            role="group"
+          >
+            <VStack
+              transition="0.3s ease"
+              py={4}
+              px="4"
+              _groupHover={{
+                bg: 'pink.500',
+                color: 'white',
+              }}
+            >
+              <Heading as="h3" size="md">
                 {item.name}
               </Heading>
-              <Text>{i + 1}X Staking Rewards</Text>
+            </VStack>
+            <VStack py={4} px="4">
+              <Text fontSize="md" textAlign="center">
+                {i + 1}X <br /> Staking Rewards
+              </Text>
               <Stack>
                 {item.benefits.map((benefit) => (
-                  <HStack key={benefit.text}>
-                    <Icon>{benefit.icon}</Icon>
-                    <Text>{benefit.text}</Text>
+                  <HStack spacing="0" key={benefit.text}>
+                    <Icon color="purple.700">{benefit.icon}</Icon>
+                    <Text fontSize="sm">{benefit.text}</Text>
                   </HStack>
                 ))}
               </Stack>
-              <Button>
-                {item.price === 0 ? 'Free' : `$${item.price} per month*`}
+              <Button
+                transition="0.3s ease"
+                _hover={{ opacity: 0.8 }}
+                _groupHover={{
+                  bg: 'pink.500',
+                  color: 'white',
+                }}
+                variant="outline"
+                w="80%"
+              >
+                {item.price === 0 ? (
+                  'Free'
+                ) : (
+                  <>
+                    {`$${item.price} per month*`}
+                    <Icon>
+                      <FaArrowRight />
+                    </Icon>
+                  </>
+                )}
               </Button>
             </VStack>
           </GridItem>
