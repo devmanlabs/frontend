d5168e2b81364cc098aeb4a5a10522125278b87c
Anshu Raj <anshusopinion@gmail.com>
2022-03-23 07:56:46 +0530

    sidebar layout added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,13 @@
-import { Button, Flex, Grid, GridItem, Stack, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 
 import { Card } from '~/components/Card';
 import BalanceCard from '~/components/Home/BalanceCard';
@@ -13,61 +22,67 @@ const Home = () => {
       <Grid placeItems="center" as="nav" height="80px" bg="gray.100">
         Header
       </Grid>
-      <Flex minH="100vh" w="100%">
+      <Flex>
         <Stack
           h="100%"
           w="350px"
+          bg="white"
+          zIndex="100"
+          pos="absolute"
+          top="80px"
           boxShadow="2xl"
-          display={{ base: 'none', md: 'none' }}
+          display={{ base: 'none', md: 'flex' }}
         >
           <Text>hi</Text>
         </Stack>
-        <Stack w="100%" spacing={0}>
-          <Grid
-            padding="4"
-            templateRows={{ sm: 'repeat(8, 1fr)', lg: 'repeat(7, 1fr)' }}
-            templateColumns={{ sm: 'repeat(8, 1fr)', lg: 'repeat(9, 1fr)' }}
-            width="full"
-            flex="1"
-            gap="4"
-            bg="gray.50"
-          >
-            <GridItem colSpan={{ sm: 8, lg: 5 }} rowSpan={{ sm: 2, lg: 2 }}>
-              <BalanceCard />
-            </GridItem>
-            <GridItem colSpan={{ sm: 4, lg: 2 }} rowSpan={{ sm: 2, lg: 2 }}>
-              <RewardCard />
-            </GridItem>
-            <GridItem colSpan={{ sm: 4, lg: 2 }} rowSpan={{ sm: 2, lg: 2 }}>
-              <RemainingDaysCard />
-            </GridItem>
+        <Box minW="100%">
+          <Flex w="full" minH="100%">
+            <Grid
+              padding="4"
+              templateRows={{ sm: 'repeat(8, 1fr)', lg: 'repeat(7, 1fr)' }}
+              templateColumns={{ sm: 'repeat(8, 1fr)', lg: 'repeat(9, 1fr)' }}
+              width="full"
+              flex="1"
+              gap="4"
+              bg="gray.50"
+            >
+              <GridItem colSpan={{ sm: 8, lg: 5 }} rowSpan={{ sm: 1, lg: 2 }}>
+                <BalanceCard />
+              </GridItem>
+              <GridItem colSpan={{ sm: 4, lg: 2 }} rowSpan={{ sm: 1, lg: 2 }}>
+                <RewardCard />
+              </GridItem>
+              <GridItem colSpan={{ sm: 4, lg: 2 }} rowSpan={{ sm: 1, lg: 2 }}>
+                <RemainingDaysCard />
+              </GridItem>
 
-            <GridItem colSpan={{ sm: 3, lg: 2 }} rowSpan={{ sm: 4, lg: 5 }}>
-              <MembershipDetailsCard />
-            </GridItem>
-            <GridItem
-              w="99%"
-              colSpan={{ sm: 5, lg: 7 }}
-              rowSpan={{ sm: 4, lg: 5 }}
-              as={Card}
-            >
-              <LineChartPreview />
-            </GridItem>
-            <GridItem
-              colStart={{ sm: 7, lg: 8 }}
-              colEnd={{ sm: 9, lg: 10 }}
-              display="flex"
-              justifyContent="center"
-            >
-              <Button colorScheme="pink" w="full" mx="2">
-                Mint New Key
-              </Button>
-            </GridItem>
-          </Grid>
+              <GridItem colSpan={{ sm: 3, lg: 2 }} rowSpan={{ sm: 4, lg: 5 }}>
+                <MembershipDetailsCard />
+              </GridItem>
+              <GridItem
+                w="99%"
+                colSpan={{ sm: 5, lg: 7 }}
+                rowSpan={{ sm: 4, lg: 5 }}
+                as={Card}
+              >
+                <LineChartPreview />
+              </GridItem>
+              <GridItem
+                colStart={{ sm: 7, lg: 8 }}
+                colEnd={{ sm: 9, lg: 10 }}
+                display="flex"
+                justifyContent="center"
+              >
+                <Button colorScheme="pink" w="full" mx="2">
+                  Mint New Key
+                </Button>
+              </GridItem>
+            </Grid>
+          </Flex>
           <Grid placeItems="center" as="nav" height="80px" bg="gray.100">
             Footer
           </Grid>
-        </Stack>
+        </Box>
       </Flex>
     </Stack>
   );
