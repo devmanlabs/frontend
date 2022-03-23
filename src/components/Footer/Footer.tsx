3ea8b9daff98ca4034cba9a03b796465b4c14c1f
Anshu Raj <anshusopinion@gmail.com>
2022-03-23 12:34:48 +0530

    Footer Done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,23 @@
 import {
   Box,
   Container,
+  Flex,
+  HStack,
   Link,
   SimpleGrid,
   Stack,
   Text,
 } from '@chakra-ui/react';
 import { FC, ReactNode } from 'react';
+import {
+  FaDiscord,
+  FaGithub,
+  FaReddit,
+  FaTelegram,
+  FaTwitter,
+  FaWhatsapp,
+  FaYoutube,
+} from 'react-icons/fa';
 
 import Logo from '../Logo/Logo';
 
@@ -21,57 +32,119 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = () => {
   const footerRoutes = [
     {
-      header: 'Company',
-      routes: [{}],
+      header: 'Projects',
+      routes: [
+        {
+          name: 'White Paper',
+          link: '/',
+        },
+        {
+          name: 'Library',
+          link: '/',
+        },
+        {
+          name: 'Reports',
+          link: '/',
+        },
+        {
+          name: 'Terminal',
+          link: '/',
+        },
+      ],
+    },
+    {
+      header: 'Learn',
+      routes: [
+        {
+          name: 'Blog',
+          link: '/',
+        },
+        {
+          name: 'Media',
+          link: '/',
+        },
+        {
+          name: 'FAQ',
+          link: '/',
+        },
+      ],
+    },
+    {
+      header: 'Community',
+      routes: [
+        {
+          name: 'Contributors',
+          link: '/',
+        },
+        {
+          name: 'Work with us',
+          link: '/',
+        },
+        {
+          name: 'Our Discord',
+          link: '/',
+        },
+      ],
+    },
+    {
+      header: 'Languages',
+      routes: [
+        {
+          name: 'English',
+          link: '/',
+        },
+        {
+          name: 'English',
+          link: '/',
+        },
+        {
+          name: 'English',
+          link: '/',
+        },
+      ],
     },
   ];
 
   return (
     <Box bg="purple.600" color={'gray.200'}>
       <Container as={Stack} maxW={'6xl'} py={10}>
-        <SimpleGrid
-          templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 1fr 1fr' }}
-          spacing={8}
-        >
+        <Flex justify="space-between" flexDir={{ base: 'column', md: 'row' }}>
           <Stack spacing={6}>
             <Box>
               <Logo />
             </Box>
-            <Text fontSize={'sm'}>© 2022 Devmanlabs All rights reserved</Text>
+            <HStack spacing="6" fontSize="3xl">
+              <FaDiscord />
+              <FaTwitter />
+              <FaYoutube />
+              <FaGithub />
+              <FaTelegram />
+              <FaReddit />
+              <FaWhatsapp />
+            </HStack>
           </Stack>
-          <Stack align={'flex-start'}>
-            <ListHeader>Product</ListHeader>
-            <Link href={'#'}>Overview</Link>
-            <Link href={'#'}>Features</Link>
-            <Link href={'#'}>Tutorials</Link>
-            <Link href={'#'}>Pricing</Link>
-            <Link href={'#'}>Releases</Link>
-          </Stack>
-          <Stack align={'flex-start'}>
-            <ListHeader>Company</ListHeader>
-            <Link href={'#'}>About</Link>
-            <Link href={'#'}>Press</Link>
-            <Link href={'#'}>Careers</Link>
-            <Link href={'#'}>Contact</Link>
-            <Link href={'#'}>Partners</Link>
-          </Stack>
-          <Stack align={'flex-start'}>
-            <ListHeader>Support</ListHeader>
-            <Link href={'#'}>Help Center</Link>
-            <Link href={'#'}>Terms of Service</Link>
-            <Link href={'#'}>Legal</Link>
-            <Link href={'#'}>Privacy Policy</Link>
-            <Link href={'#'}>Status</Link>
-          </Stack>
-          <Stack align={'flex-start'}>
-            <ListHeader>Follow Us</ListHeader>
-            <Link href={'#'}>Facebook</Link>
-            <Link href={'#'}>Twitter</Link>
-            <Link href={'#'}>Dribbble</Link>
-            <Link href={'#'}>Instagram</Link>
-            <Link href={'#'}>LinkedIn</Link>
-          </Stack>
-        </SimpleGrid>
+          <SimpleGrid
+            my={{ base: 10, md: 0 }}
+            templateColumns={{ base: '1fr 1fr', md: ' 1fr 1fr 1fr 1fr' }}
+            spacing={8}
+          >
+            {footerRoutes.map((footerRoute) => (
+              <Stack key={footerRoute.header} align={'flex-start'}>
+                <ListHeader>{footerRoute.header}</ListHeader>
+                {footerRoute.routes.map((route) => (
+                  <Link
+                    key={route.name}
+                    href={route.link}
+                    fontSize={'sm'}
+                    color={'gray.500'}
+                  >
+                    {route.name}
+                  </Link>
+                ))}
+              </Stack>
+            ))}
+          </SimpleGrid>
+        </Flex>
       </Container>
     </Box>
   );
